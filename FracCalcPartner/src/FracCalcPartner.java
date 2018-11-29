5623245f4ee27ede93fe8a0269ca223eeda0c517
Baileys3 <42974372+Baileys3@users.noreply.github.com>
2018-11-29 09:18:47 -0800

    Godly YEETUS!

--- FracCalcPartner/src/FracCalcPartner.java
+++ FracCalcPartner/src/FracCalcPartner.java
@@ -42,14 +42,27 @@ public class FracCalcPartner {
 					return wholeAns + "";
 				}
 			}
-				if (wholeAns != 0) {
-					return wholeAns + "_" + numerAns + "/" + denomAns; 
-				} else { 
-					return numerAns + "/" + denomAns;
-				}
+			if (wholeAns != 0) {
+				return wholeAns + "_" + numerAns + "/" + denomAns; 
+			} else { 
+				return numerAns + "/" + denomAns;
+			}
 
 		} else if (symbol.contains("*") || symbol.contains("/")) {
-
+			int wholeAns;
+			int numerAns = numerAnsMult(whole1, numerator1, denominator1, symbol, whole2, numerator2, denominator2);
+			int denomAns = denomAnsMult(denominator1, symbol, whole2, numerator2, denominator2);
+			if (numerAns >= denomAns) {
+				wholeAns = (numerAns / denomAns);
+				numerAns = numerAns % denomAns;
+				if (numerAns == 0) {
+					return wholeAns + "";
+				} else {
+					return wholeAns + "_" + numerAns + "/" + denomAns;
+				}
+			} else {
+				return numerAns + "/" + denomAns;
+			}
 		}
 		return null;
 	}
@@ -63,57 +76,82 @@ public class FracCalcPartner {
 		return 0;
 	}
 
-public static int numerNum(String frac) {
-	if (frac.contains("/")) {
-		return Integer.parseInt(frac.substring(frac.indexOf('_') + 1, frac.indexOf('/')));
-	} else {
+	public static int numerNum(String frac) {
+		if (frac.contains("/")) {
+			return Integer.parseInt(frac.substring(frac.indexOf('_') + 1, frac.indexOf('/')));
+		} else {
+			return 0;
+		}
+
+	}
+
+	public static int denomNum(String frac) {
+		if (frac.contains("/")) {
+			return Integer.parseInt(frac.substring(frac.indexOf('/') + 1, frac.length())); 
+		} else {
+			return 1;
+		}
+
+	}
+
+	public static int wholeAnsAdd(int whole1, String symbol, int whole2) {
+		if (symbol.contains("+")) {
+			return whole1 + whole2;
+		} else if (symbol.contains("-")) {
+			if (whole2 < 0) {
+				whole2 = Math.abs(whole2);
+				return whole1 + whole2;
+			} else {
+				return whole1 - whole2;
+			} 
+		}
+		return 0;
+	}
+	public static int numerAnsAdd(int numer1, int denom1, String symbol, int numer2, int denom2) {
+		if (denom1 == denom2) {
+			if (symbol.contains("+")) {
+				return numer1 + numer2;
+			} else if (symbol.contains("-")) {
+				return numer1 - numer2;
+			} 
+		} else {
+			numer2 *= denom1;
+			numer1 *= denom2;
+			if (symbol.contains("+")) {
+				return numer1 + numer2;
+			} else if (symbol.contains("-")) {
+				return numer1 - numer2;
+			}
+		}
 		return 0;
 	}
 
-}
-
-public static int denomNum(String frac) {
-	if (frac.contains("/")) {
-		return Integer.parseInt(frac.substring(frac.indexOf('/') + 1, frac.length())); 
-	} else {
-		return 1;
-	}
-
-}
-
-public static int wholeAnsAdd(int whole1, String symbol, int whole2) {
-	if (symbol.contains("+")) {
-		return whole1 + whole2;
-	} else if (symbol.contains("-")) {
-		return whole1 - whole2;
-	} return 0;
-}
-public static int numerAnsAdd(int numer1, int denom1, String symbol, int numer2, int denom2) {
-	if (denom1 == denom2) {
-		if (symbol.contains("+")) {
-			return numer1 + numer2;
-		} else if (symbol.contains("-")) {
-			return numer1 - numer2;
-		} 
-	} else {
-		numer2 *= denom1;
-		numer1 *= denom2;
-		if (symbol.contains("+")) {
-			return numer1 + numer2;
-		} else if (symbol.contains("-")) {
-			return numer1 - numer2;
+	public static int denomAnsAdd(int denom1, String symbol, int denom2) {
+		if (denom1 == denom2) {
+			return denom1;
+		} else {
+			return denom1 * denom2;
 		}
 	}
-	return 0;
-}
 
-public static int denomAnsAdd(int denom1, String symbol, int denom2) {
-	if (denom1 == denom2) {
-		return denom1;
-	} else {
-		return denom1 * denom2;
+	public static int numerAnsMult(int whole1, int numer1, int denom1, String symbol, int whole2, int numer2, int denom2) {
+		numer1 = (denom1 * whole1) + numer1;
+		numer2 = (denom2 * whole2) + numer2;
+		if (symbol.contains("*")) {
+			return numer1 * numer2;
+		} else { 
+			return numer1 * denom2;
+		}
 	}
-}
+
+	public static int denomAnsMult(int denom1, String symbol, int whole2, int numer2, int denom2) {
+		if (symbol.contains("*") ) {
+			return denom1 * denom2;
+		} else {
+			numer2 = (denom2 * whole2) + numer2;
+			return denom1 * numer2;
+		}
+	}
 }
 
 
